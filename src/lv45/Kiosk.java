436681912ef48909f0ca43af45a5670a2aad4991
thezz9 <thezz9@naver.com>
2025-03-07 20:38:15 +0900

    Feat: 레벨 4, 5 구현 완료 date: 25/03/07 1. Menu 클래스 생성 (List<MenuItem> 관리 및 카테고리 필드 추가) 2. Kiosk 클래스에서 전체 흐름 관리 3. main 함수에서 Menu 객체를 생성 후 Kiosk 실행 4. 입력값 검증 (getValidInput() 함수 활용) 5. 뒤로가기 기능 추가 6. 캡슐화 적용

---
+++ src/lv45/Kiosk.java
@@ -0,0 +1,76 @@
+package lv45;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+
+    private final List<Menu> menus;
+
+    public Kiosk() {
+        this.menus = new ArrayList<>();
+    }
+
+    public void addMenu(Menu menu) {
+        menus.add(menu);
+    }
+
+    public void start() {
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("\n[ MAIN MENU ]");
+            for (int i = 0; i < menus.size(); i++) {
+                System.out.println((i + 1) + ". " + menus.get(i).getCategory());
+            }
+            System.out.println("0. 종료      | 종료");
+
+            // 사용자 입력 받기
+            System.out.print("카테고리 번호를 입력하세요: ");
+            int categoryChoice = getValidInput(sc, menus.size());
+
+            if (categoryChoice == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            Menu selectedMenu = menus.get(categoryChoice - 1);
+            menuSelection(sc, selectedMenu);
+
+        } while (true);
+    }
+    private void menuSelection(Scanner sc, Menu menu) {
+        do {
+            menu.displayItems();
+            System.out.print("메뉴 번호를 입력하세요: ");
+            int itemChoice = getValidInput(sc, menu.getMenuItems().size());
+
+            if (itemChoice == 0) {
+                return; // 뒤로 가기
+            }
+
+            MenuItem selectedItem = menu.getMenuItems().get(itemChoice - 1);
+            System.out.println("선택한 메뉴: " + selectedItem.getName() + " | W " +
+                    selectedItem.getPrice() + " | " + selectedItem.getDescription());
+        } while (true);
+    }
+
+    private int getValidInput(Scanner sc, int maxOption) {
+        int categoryChoice;
+        while (true) {
+            if (!sc.hasNextInt()) {
+                System.out.println("올바른 숫자를 입력하세요.");
+                sc.next();
+                continue;
+            }
+            categoryChoice = sc.nextInt();
+            if (categoryChoice >= 0 && categoryChoice <= maxOption) {
+                return categoryChoice;
+            } else {
+                System.out.println("올바른 메뉴 번호를 입력하세요.");
+            }
+        }
+    }
+}
---
+++ src/lv45/Menu.java
@@ -0,0 +1,38 @@
+package lv45;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+
+    private final String category;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String category) {
+        this.category = category;
+        // 생성자가 호출될 때마다 새로운 리스트 객체 생성
+        this.menuItems = new ArrayList<>();
+    }
+
+    public void addMenuItem(MenuItem item) {
+        menuItems.add(item);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void displayItems() {
+        System.out.println("[ " + category + " MENU ]");
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem item = menuItems.get(i);
+            System.out.println((i + 1) + ". " + item.getName() + " | W "
+                    + item.getPrice() + " | " + item.getDescription());
+        }
+        System.out.println("0. 뒤로가기");
+    }
+}
